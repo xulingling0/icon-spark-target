5a5a4b9eb6e32c4b2442babb0f340609c2ecb0e5
xulingling0 <107020065+xulingling0@users.noreply.github.com>
2023-08-22 17:15:41 +0800

    feat(Icons):🌟 新增 `Layout` `Status`

--- packages/icons/src/components/outline/index.tsx
+++ packages/icons/src/components/outline/index.tsx
@@ -1,4 +1,6 @@
 export { default as Action } from './Action'
 export { default as Add } from './Add'
+export { default as Layout } from './Layout'
+export { default as Status } from './Status'
 export { default as Todo } from './Todo'
 export { default as ZoomOut } from './ZoomOut'
